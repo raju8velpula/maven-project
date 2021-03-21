da4c43e9fc45f48d96104c7c29374f4d93f74350
rajuvelpula <raju8velpula@gmail.com>
2021-03-21 19:22:02 +0530

    maven project

---
+++ src/test/java/project/Tech/satic.java
@@ -0,0 +1,14 @@
+package project.Tech;
+
+public class satic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "nagaraju";
+		
+		for(int i = str.length()-1 ; i>=0 ;i--) {
+			System.out.println(str.charAt(i));
+		}
+	}
+
+}
